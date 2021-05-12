e66eb920b84d9310b1f7c3958301439e6185815e
Łukasz Szypski <250094@student.pwr.edu.pl>
2021-05-12 02:44:20 +0200

    obsluga przepisu

--- app/src/main/java/com/example/asystentgotowania/ui/search/RecipeDetailActivity.kt
+++ app/src/main/java/com/example/asystentgotowania/ui/search/RecipeDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.example.asystentgotowania.ui.search
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.asystentgotowania.R
 
@@ -11,5 +13,21 @@ class RecipeDetailActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
         //TODO parse recipe from intent
+        findViewById<TextView>(R.id.name)
+        findViewById<TextView>(R.id.time)
+        findViewById<TextView>(R.id.size)
+        findViewById<TextView>(R.id.level)
+        var e = findViewById<ImageView>(R.id.recipe_image)
+
+        findViewById<TextView>(R.id.ingredients).text = createIngredients(arrayListOf())
+        findViewById<TextView>(R.id.recipe)
+    }
+
+    private fun createIngredients(list: ArrayList<String>): String {
+        var stringList = ""
+        for (i in list) {
+            stringList += "•   $list\n"
+        }
+        return stringList
     }
 }
